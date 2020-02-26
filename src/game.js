ddad2d27a08f9d6158e6c35861486d36f6365445
Carlos Sánchez Álvarez <60895026+carsanchezalv@users.noreply.github.com>
2020-02-26 18:44:49 +0100

    prueba fondo

--- src/game.js
+++ src/game.js
@@ -3,6 +3,14 @@ export default class Game extends Phaser.Scene {
     super({ key: 'main' });
   }
   preload() {  
+   //   this.load.image("logo", "assets/favicon.png")
+        this.load.image("fondo", "assets/web/portada.png")
+   //     this.load.image("button_play", "assets/textures/gui/menu_button_play.png")
+   //     this.load.image("button_exit", "assets/textures/gui/menu_button_exit.png")
+   //     this.load.audio("musica_inicio", [
+  //          "assets/audio/bgm/menu_background.ogg",
+   //         "assets/audio/bgm/menu_background.mp3"
+   //     ]);
   }
 
   create() {
