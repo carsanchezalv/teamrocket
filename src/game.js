0509190dfefacee77b5ba64ed240c34af09f1f8f
Carlos Sanchez Alvarez <carsanchezalv@users.noreply.github.com>
2020-02-27 18:00:59 +0100

    try

--- src/game.js
+++ src/game.js
@@ -4,12 +4,12 @@ export default class Game extends Phaser.Scene {
   }
   preload() {  
    //   this.load.image("logo", "assets/favicon.png")
-        this.load.image("fondo", "portada.png")
+        this.load.image("fondo", "assets/textures/portada.png")
    //     this.load.image("button_play", "assets/textures/gui/menu_button_play.png")
    //     this.load.image("button_exit", "assets/textures/gui/menu_button_exit.png")
        this.load.audio("musica_portada", [
-            "./assets/music/portada.ogg",
-            "./assets/music/portada.mp3"
+            "assets/music/portada.ogg",
+            "assets/music/portada.mp3"
         ]);
   }
 
