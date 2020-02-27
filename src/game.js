3e40a139c908da13d97f46290a84641d430367e4
Carlos Sanchez Alvarez <carsanchezalv@users.noreply.github.com>
2020-02-27 17:54:03 +0100

    musica intento

--- src/game.js
+++ src/game.js
@@ -7,14 +7,28 @@ export default class Game extends Phaser.Scene {
         this.load.image("fondo", "portada.png")
    //     this.load.image("button_play", "assets/textures/gui/menu_button_play.png")
    //     this.load.image("button_exit", "assets/textures/gui/menu_button_exit.png")
-   //     this.load.audio("musica_inicio", [
-  //          "assets/audio/bgm/menu_background.ogg",
-   //         "assets/audio/bgm/menu_background.mp3"
-   //     ]);
+       this.load.audio("musica_portada", [
+            "./assets/music/portada.ogg",
+            "./assets/music/portada.mp3"
+        ]);
   }
 
   create() {
+    //Portada
     this.add.image(400, 300, "fondo");
+
+    //Musica
+    let a_config = {
+      mute: false,
+      volume: 0.3,
+      rate: 1,
+      detune: 0,
+      seek: 0,
+      loop: true,
+      delay: 0
+  };
+  let music = this.sound.add('medieval_m', a_config);
+  music.play();
   }
   update(time, delta) {    
   }
