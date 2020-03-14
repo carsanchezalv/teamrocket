d9be43f5fc97c564fc1298162359a5f2e2e29937
Carlos Sanchez Alvarez <carsanchezalv@users.noreply.github.com>
2020-03-14 22:45:42 +0100

    sprite herencia

--- src/game.js
+++ src/game.js
@@ -1,5 +1,5 @@
 
-import Pikachu from "./player.js";
+import Pikachu from "./pikachu.js";
 export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: 'main' });
@@ -7,24 +7,26 @@ export default class Game extends Phaser.Scene {
 
   preload() {  
    //   this.load.image("logo", "assets/favicon.png")
-        this.load.image("fondo", "assets/textures/mapaportada.png")
-   //     this.load.image("button_play", "assets/textures/gui/menu_button_play.png")
-   //     this.load.image("button_exit", "assets/textures/gui/menu_button_exit.png")
-       this.load.audio("musica_portada", [
-            "assets/music/portada.ogg",
-            "assets/music/portada.mp3"
-        ]);
-        this.load.spritesheet('protagonista', 'assets/icons/personajes/Protagonista/8/25.png',{ frameWidth: 48, frameHeight: 64 });
+    this.load.image("fondo", "assets/textures/mapaportada.png")
+  //     this.load.image("button_play", "assets/textures/gui/menu_button_play.png")
+  //     this.load.image("button_exit", "assets/textures/gui/menu_button_exit.png")
+    this.load.audio("musica_portada", [
+      "assets/music/portada.ogg",
+      "assets/music/portada.mp3"
+    ]);
+    this.load.spritesheet('protagonista', 'assets/icons/personajes/Protagonista/8/25.png',{ frameWidth: 48, frameHeight: 64 });
   }
 
   create() {
-    //Portada
    
+    // Portada
     this.add.image(700, 400, "fondo");
     let x = 500;
     let y = 200;
-    this.pikachu = new Pikachu(this, x, y);
-    //Musica
+    this.pikachuSprite = new Pikachu(this, x, y);
+    this.add.existing(this.pikachuSprite);
+
+    // Música
     let config = {
       mute: false,
       volume: 0.3,
@@ -36,10 +38,20 @@ export default class Game extends Phaser.Scene {
     };
     let music = this.sound.add('musica_portada', config);
     music.play();
-
+    
+    /*
+    this.add
+    .text(16, 16, `Derrota a los guardianes de cada territorio.\nTerritorio actual: ${this.territorio}.\nGuardianes restantes: ${this.guardianes}`, {
+        font: "18px Press Start 2P",
+        fill: "#E60026",
+        padding: { x: 20, y: 20 },
+        backgroundColor: "#fff88f"
+    })     
+    .setScrollFactor(0);
+    */
   }
 
   update(time, delta) {
-    this.pikachu.update();
+    this.pikachuSprite.preUpdate(time, delta);
   }
 }
---
+++ src/pikachu.js
@@ -0,0 +1,277 @@
+export default class Pikachu extends Phaser.GameObjects.Sprite {
+
+  constructor(scene, x, y) {
+    super(scene, x, y, 'phaser');
+      //Protagonista 
+    this.scene = scene;
+    let orientation = "down";
+    this.animation = "move_down";
+    this.anims = scene.anims;
+  //  let x = x;
+  //  let y = y;
+    
+   
+    
+    // Animacion movimientos
+    this.anims.create({
+      key: 'move_down',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 1, end: 2 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: 'move_right',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 7, end: 8 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: 'move_up',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 13, end: 14 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: 'move_upright',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 10, end: 11 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: 'move_downright',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 4, end: 5 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+// Animacion ataque
+    this.anims.create({
+      key: 'attack_down',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 32, end: 34 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: 'attack_right',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 40, end: 42 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: 'attack_up',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 48, end: 50 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: 'attack_upright',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 44, end: 46 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: 'attack_downright',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 36, end: 38 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+// Animacion evolucion 
+    this.anims.create({
+      key: 'evolve',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 52, end: 54 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+// Animaciones daño
+   this.anims.create({
+      key: 'damage_down',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 17, end: 17 }),
+      frameRate: 2,
+      repeat: 0
+    }); 
+
+    this.anims.create({
+      key: 'damage_downright',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 18, end: 18 }),
+      frameRate: 2,
+      repeat: 0
+    });
+    this.anims.create({
+      key: 'damage_right',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 19, end: 19 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: 'damage_upright',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 20, end: 20 }),
+      frameRate: 2,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: 'damage_up',
+      frames: this.anims.generateFrameNumbers('protagonista', { start: 21, end: 21 }),
+      frameRate: 2,
+      repeat: 0
+    });
+    scene.add.existing(this);
+    scene.play(this.animation, true);
+    this.cursor = this.scene.input.keyboard.createCursorKeys();
+  }
+
+  preUpdate(t, dt) {
+//    super this.preUpdate(t, dt);
+    const cursor = this.cursor;
+
+    const velocidad = 1;
+  //  let this = this.this;
+    
+    let scene = this.scene;
+
+    if(this.cursor.space.isDown)
+    {
+      if(this.orientation === "down")
+      {  
+        this.animation = "attack_down";
+        this.anims.play(this.animation, true);
+    //    sprite.anims.play(this.animation, true);
+  //     this.anims.play("attack_down", true);
+      }
+      else if(this.orientation === "right")
+      {
+        this.animation = "attack_right";
+        
+        this.flipX = false;
+        this.anims.play(this.animation, true);
+  //     this.anims.play("attack_right", true);
+      }
+      else if(this.orientation === "up")
+      {
+        this.animation = "attack_up";
+        this.anims.play(this.animation, true);
+  //     this.anims.play("attack_up", true);
+      }
+      else if(this.orientation === "left")
+      {
+        this.animation = "attack_right";
+        this.flipX = true;
+        this.anims.play(this.animation, true);
+  //     this.anims.play("attack_right", true);
+      }
+      else if(this.orientation === "upright")
+      {
+        this.animation = "attack_upright";
+        this.flipX = false;
+        this.anims.play(this.animation, true);
+    //    this.anims.play("attack_upright", true);
+      }
+      else if(this.orientation === "upleft")
+      {
+        this.animation = "attack_upright";
+        this.flipX = true;
+        this.anims.play(this.animation, true);
+    //    this.anims.play("attack_upright", true);
+      }
+      else if(this.orientation === "downright")
+      {
+        this.animation = "attack_downright";
+        this.flipX = false;
+        this.anims.play(this.animation, true);
+    //    this.anims.play("attack_downright", true);
+      }
+      else if(this.orientation === "downleft")
+      {
+        this.animation = "attack_downright";
+        this.flipX = true;
+        this.anims.play(this.animation, true);
+    //    this.anims.play("attack_downright", true);
+      }
+    }
+    else if(cursor.up.isDown && cursor.right.isDown)
+    {
+      this.x++;
+      this.y--;
+      this.orientation = "upright";
+      this.flipX = false;
+      this.animation = "move_upright";
+      this.anims.play(this.animation, true);
+    //  this.anims.play("move_upright", true);
+    }
+    else if(cursor.up.isDown && cursor.left.isDown)
+    {
+      this.x--;
+      this.y--;
+      this.orientation = "upleft";
+      this.flipX = true;
+      this.animation = "move_upright";
+      this.anims.play(this.animation, true);
+    //  this.anims.play("move_upright", true);
+    }
+    else if(cursor.down.isDown && cursor.right.isDown)
+    {
+      this.x++;
+      this.y++;
+      this.orientation = "downright";
+      this.flipX = false;
+      this.animation = "move_downright";
+      this.anims.play(this.animation, true);
+    //  this.anims.play("move_downright", true);
+    }
+    else if(cursor.down.isDown && cursor.left.isDown)
+    {
+      this.x--;
+      this.y++;
+      this.orientation = "downleft";
+      this.flipX = true;
+      this.animation = "move_downright";
+      this.anims.play(this.animation, true);
+  //   this.anims.play("move_downright", true);
+    }
+    else if(cursor.right.isDown)
+    {
+      this.x++;
+      this.orientation = "right";
+      this.flipX = false;
+      this.animation = "move_right";
+      this.anims.play(this.animation, true);
+  //   this.anims.play("move_right", true);
+    }
+    else if(cursor.left.isDown)
+    {
+      this.x--;
+      this.orientation = "left";
+      this.flipX = true;
+      this.animation = "move_right";
+      this.anims.play(this.animation, true);
+    //  this.anims.play("move_right", true);
+    }
+    else if(cursor.up.isDown)
+    {
+      this.y--;
+      this.orientation = "up";
+      this.animation = "move_up";
+      this.anims.play(this.animation, true);
+  //   this.anims.play("move_up", true)
+    }
+    else if(cursor.down.isDown)
+    {
+      this.y++;
+      this.orientation = "down";
+      this.animation = "move_down";
+  //   this.anims.play("move_down", true);
+      this.anims.play(this.animation, true);
+    }
+  }
+}
