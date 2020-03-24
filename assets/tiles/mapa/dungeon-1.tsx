87000fdb2cd74181be1ba376fa367620aa3757dc
Carlos Sanchez Alvarez <carsanchezalv@users.noreply.github.com>
2020-03-24 13:06:29 +0100

    tiled

--- assets/tiles/mapa/dungeon-1.tsx
+++ assets/tiles/mapa/dungeon-1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.2" name="dungeon-1" tilewidth="24" tileheight="24" tilecount="432" columns="18">
- <image source="../Documents/GitHub/Videogame/assets/tiles/dungeon-1.png" width="432" height="576"/>
+ <image source="../dungeon-1.png" width="432" height="576"/>
 </tileset>
