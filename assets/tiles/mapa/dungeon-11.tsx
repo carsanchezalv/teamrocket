0b8023190329156a731ead08be4767183f06b25c
Joel Garcia Aparicio <jogarciael@gmail.com>
2020-03-24 01:00:46 +0100

    mapa1

---
+++ assets/tiles/mapa/dungeon-11.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="dungeon-11" tilewidth="24" tileheight="24" tilecount="432" columns="18">
+ <image source="../Documents/GitHub/Videogame/assets/tiles/dungeon-11.png" width="432" height="576"/>
+</tileset>
