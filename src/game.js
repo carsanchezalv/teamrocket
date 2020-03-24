24fc5200debf228a881bdbdf800fe5b475b55708
Carlos Sanchez Alvarez <carsanchezalv@users.noreply.github.com>
2020-03-24 20:24:36 +0100

    mapa grande

--- src/game.js
+++ src/game.js
@@ -1,4 +1,3 @@
-
 import Pikachu from "./pikachu.js";
 export default class Game extends Phaser.Scene {
   constructor() {
@@ -7,9 +6,33 @@ export default class Game extends Phaser.Scene {
 
   preload() {  
    //   this.load.image("logo", "assets/favicon.png")
-    this.load.image("fondo", "assets/textures/mapaportada.png")
-  //     this.load.image("button_play", "assets/textures/gui/menu_button_play.png")
-  //     this.load.image("button_exit", "assets/textures/gui/menu_button_exit.png")
+
+    this.load.tilemapTiledJSON('tilemap', 'assets/tiles/mapa/mapa_team_rocket.json');
+      
+    // Tilesets
+    this.load.image('dungeon0', 'assets/tiles/dungeon-0.png');
+    this.load.image('dungeon1', 'assets/tiles/dungeon-1.png');
+    this.load.image('dungeon4', 'assets/tiles/dungeon-4.png');
+    this.load.image('dungeon6', 'assets/tiles/dungeon-6.png');
+    this.load.image('dungeon8', 'assets/tiles/dungeon-8.png');
+    this.load.image('dungeon9', 'assets/tiles/dungeon-9.png');
+    this.load.image('dungeon10', 'assets/tiles/dungeon-10.png');
+    this.load.image('dungeon11', 'assets/tiles/dungeon-11.png');
+    this.load.image('dungeon12', 'assets/tiles/dungeon-12.png');
+    this.load.image('dungeon13', 'assets/tiles/dungeon-13.png');
+    this.load.image('dungeon14', 'assets/tiles/dungeon-14.png');
+    this.load.image('dungeon33', 'assets/tiles/dungeon-33.png');
+    this.load.image('dungeon42', 'assets/tiles/dungeon-42.png');
+    this.load.image('dungeon54', 'assets/tiles/dungeon-54.png');
+
+    this.map = this.make.tilemap({ 
+      key: 'tilemap', 
+      tileWidth: 64, 
+      tileHeight: 64 
+    });
+
+ //   this.load.image("fondo", "assets/textures/mapaportada.png")
+
     this.load.audio("musica_portada", [
       "assets/music/portada.ogg",
       "assets/music/portada.mp3"
@@ -20,37 +43,51 @@ export default class Game extends Phaser.Scene {
   create() {
    
     // Portada
-    this.add.image(700, 400, "fondo"); //////
+//    this.add.image(700, 400, "fondo"); //////
 
-/*  
-    this.load.tilemapTiledJSON('tilemap', 'tilemap.json');
-    this.load.image('patronesTilemap', 'images/patrones.png');
 
-    this.map = this.make.tilemap({ 
-      key: 'tilemap', 
-      tileWidth: 64, 
-      tileHeight: 64 
-    });
-
-    this.map.addTilesetImage('patrones', 'patronesTilemap');
+////////////
+    
+    this.map.addTilesetImage('dungeon-0', 'dungeon0');
+    this.map.addTilesetImage('dungeon-1', 'dungeon1');
+    this.map.addTilesetImage('dungeon-4', 'dungeon4');
+    this.map.addTilesetImage('dungeon-6', 'dungeon6');
+    this.map.addTilesetImage('dungeon-8', 'dungeon8');
+    this.map.addTilesetImage('dungeon-9', 'dungeon9');
+    this.map.addTilesetImage('dungeon-10', 'dungeon10');
+    this.map.addTilesetImage('dungeon-11', 'dungeon11');
+    this.map.addTilesetImage('dungeon-12', 'dungeon12');
+    this.map.addTilesetImage('dungeon-13', 'dungeon13');
+    this.map.addTilesetImage('dungeon-14', 'dungeon14');
+    this.map.addTilesetImage('dungeon-33', 'dungeon33');
+    this.map.addTilesetImage('dungeon-42', 'dungeon42');
+    this.map.addTilesetImage('dungeon-54', 'dungeon54');
 
     // Capas, los nombres han de coincidir con los de las capas reales
-    this.backgroundLayer = this.map.createStaticLayer('BackgroundLayer', [tileset1, tileset2]);
-    this.groundLayer = this.map.createStaticLayer('GroundLayer', [tileset1, tileset2]);
-    this.foreground = this.map.createStaticLayer('Foreground', [tileset1, tileset2]);
+    this.borde = this.map.createStaticLayer('borde', ['dungeon-0', 'dungeon-8', 'dungeon-9', 'dungeon-14','dungeon-54']);
+    this.mar = this.map.createStaticLayer('mar', ['dungeon-0','dungeon-8', 'dungeon-9','dungeon-12','dungeon-14', 'dungeon-54']);
+    this.rio = this.map.createStaticLayer('rio', ['dungeon-1', 'dungeon-4','dungeon-11','dungeon-13', 'dungeon-33']);
+    this.suelo = this.map.createStaticLayer('suelo', ['dungeon-0', 'dungeon-1','dungeon-4','dungeon-8','dungeon-9','dungeon-11','dungeon-12','dungeon-13','dungeon-14', 'dungeon-33']);
 
+    
     // Físicas según capa
-    suelo.setCollisionByProperty({ colisiona: true });
+    this.borde.setCollisionByProperty({ colisiona: true });
+    this.mar.setCollisionByProperty({ colisiona: true });
+    this.rio.setCollisionByProperty({ colisiona: true });
 
     // Para que colisionen los personajes que tengan un rango de ID concreto
-    suelo.foreground.setCollisionBetween(0, 999);
-
+    this.borde.foreground.setCollisionBetween(0, 999);
+    this.mar.foreground.setCollisionBetween(0, 999);
+    this.rio.foreground.setCollisionBetween(0, 999);
     // colision por exclusion (?)
-    layer.setCollisionByExclusion([93, 94, 95, 96], true);
+//    layer.setCollisionByExclusion([93, 94, 95, 96], true);
 
     // añadir una fisica ya creada en los pasos anteriores a un jugador, siempre necesario
-    this.physics.add.collider(player, layer);
-*/
+//    this.physics.add.collider(player, layer);
+//////
+    
+
+    
 
     let x = 500;
     let y = 200;
